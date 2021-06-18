ebc69231636a4a4cb3b79c586fd4ee40062cb2a5
Kowalski42 <ALosk88@mail.ru>
2021-06-18 16:50:53 +0300

    3.4. Заголовок в файле java [#159036]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,9 +1,11 @@
 package ru.job4j;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Formatter;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        System.out.println(new SimpleDateFormat("dd.MM.YYYY").format(new Date()));
     }
 }
