aaea792529a2c2d6a998b7eb828df3ad1fe24673
Kowalski42 <ALosk88@mail.ru>
2021-06-19 19:47:14 +0300

    9.2. Checkstyle [#4123 #169158]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,9 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, double k) {
         double h = p / (2 * (k + 1)); // расчет высоты прямоугольника, зная p - периметр и k - соотношение длины к высоте
-        double L = h * k; // расчет длины прямоугольника
-        double s = L * h;
-        return s;
+        double l = h * k; // расчет длины прямоугольника
+        return l * h;
     }
 
     public static void main(String[] args) {
