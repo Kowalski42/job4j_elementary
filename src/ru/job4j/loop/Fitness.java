5d73c10bee34397177bd536f032b8e127b848320
Kowalski42 <ALosk88@mail.ru>
2021-06-21 16:39:26 +0300

    5.4. Протеиновая диета [#156313]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -2,7 +2,14 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
+        final int K_IVAN = 3;
+        final int K_NIK = 2;
         int month = 0;
+        while (ivan <= nik) {
+            ivan *= K_IVAN;
+            nik *= K_NIK;
+            month++;
+        }
         return month;
     }
 }
