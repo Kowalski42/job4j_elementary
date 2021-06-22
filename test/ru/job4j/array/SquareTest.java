2cda85dfb08cb71bbf0b6ba6d8cf82838cd5a9c9
Kowalski42 <ALosk88@mail.ru>
2021-06-22 18:47:21 +0300

    6.0.5. Массивы в тестах. [#226789 #170372]

--- test/ru/job4j/array/SquareTest.java
+++ test/ru/job4j/array/SquareTest.java
@@ -16,7 +16,7 @@ public class SquareTest {
     public void whenBound5Then014925() {
         int bound = 5;
         int[] result = Square.calculate(bound);
-        int[] expected = {0, 1, 4, 9, 16}; // можно же объявить массив для теста и так?
+        int[] expected = new int[] {0, 1, 4, 9, 16};
         Assert.assertArrayEquals(expected, result);
     }
 }
