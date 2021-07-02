7fee4030f6a6e19b8c3d0fbbac9ca6954691b924
Kowalski42 <ALosk88@mail.ru>
2021-07-02 23:56:11 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -5,23 +5,22 @@ import java.util.*;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-        int[] rsl = new int[100];
-        int size = 0; // для движения в массиве сдачи int[] rsl
-        int i = 0; // для движения в массиве монет int[] coins
-        int rest = money - price;
-        while (rest != 0) {
-            if (rest < coins[i]) {
+        int[] change = new int[100];
+        int coinCounter = 0;
+        int i = 0;
+        money -= price;
+        while (money != 0) {
+            if (money < coins[i]) {
                 i++;
             } else {
-                rsl[size] = coins[i];
-                rest -= coins[i];
-                size++;
+                change[coinCounter++] = coins[i];
+                money -= coins[i];
             }
         }
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(change, coinCounter);
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(change(25, 7)));
+        System.out.println(Arrays.toString(change(9, 7)));
     }
 }
